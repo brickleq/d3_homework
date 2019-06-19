fc08533d4a1547bd08019719edc41b198f151051
Brickey LeQuire <pbl@uchicago.edu>
2019-06-19 16:42:10 -0500

    scatter plot working--regression line not working, so forget about regression line

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,191 +1,121 @@
-// Set up an SVG chart
-//= ================================
-// var svgWidth = 960;
-// var svgHeight = 500;
+var svgWidth = 960;
+var svgHeight = 500;
 
-// var margin = {
-//   top: 20,
-//   right: 40,
-//   bottom: 60,
-//   left: 50
-// };
+var margin = {
+  top: 30,
+  right: 40,
+  bottom: 70,
+  left: 90,
+};
 
-// var width = svgWidth - margin.left - margin.right;
-// var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper,
-// append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-// =================================
-// var svg = d3
-//   .select("scatter")
-//   .append("svg")
-//   .attr("width", svgWidth)
-//   .attr("height", svgHeight);
 
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// Select #scatter, append SVG area to it, and set the dimensions
+var svg = d3.select("#scatter")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
 
-// // d3.csv("assets/data/data.csv").then(function(data) { 
 
-// //   data.forEach(d) {
-// //     d.abbr = d.abbr;
-// //     d.age = +d.age;
-// //     d.ageMoe = +d.ageMoe;
-// //     d.healthcare = +d.healthcare;
-// //     d.healthcareHigh = +d.healthcareHigh;
-// //     d.id = +d.id;
-// //     d.income = +d.income;
-// //     d.incomeMoe = +d.incomeMoe;
-// //     d.obesity = +d.obesity;
-// //     d.obesityHigh = +d.obesityHigh;
-// //     d.obesityLow = +d.obesityLow;
-// //     d.poverty = +d.poverty;
-// //     d.povertyMoe = +d.povertyMoe;
-// //     d.smokes = +d.smokes;
-// //     d.smokesHigh = +d.smokesHigh;
-// //     d.smokesLow = +d.smokesLow;
-// //     d.state = d.state;
-// //   }
-// //     return(d);
-// //   );
-// //   var data = d;
-  
-// //   return data;
-// // }
+// Append a group to the SVG area and shift ('translate') it to the right and down
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// THIS PART IS WORKING--OK TO UNCOMMENT:
-// var data = d3.csv("assets/data/data.csv").then(function(data)
-//    {return data;
-//    });
-// console.log(data); // shows up
-// AD SE IPSUM: I only need to get IDs, X, and Y values!
 
-var data = [];
-// load data
-var data = d3.csv("assets/data/data.csv").then(function(data) {
+// Import data from data.csv
+d3.csv("assets/data/data.csv", function(error, data) {
+  if (error) throw error;
+ 
+  console.log(data);
 
-  // change string (from CSV) into number format
+  //Cast the values to numbers
   data.forEach(function(d) {
-    d.abbr = d.abbr;
-    d.obesity = +d.obesity;
     d.poverty = +d.poverty;
     d.smokes = +d.smokes;
-    console.log(d);
+        
   });
+  
+//Create scale functions
+
+var xLinearScale = d3.scaleLinear()
+  .domain([d3.min(data, d => d.poverty)-0.5, d3.max(data, d => d.poverty)+0.5])
+  .range([0, width]);
+
+var yLinearScale = d3.scaleLinear()
+  .domain([d3.min(data, d => d.smokes)-1, d3.max(data, d => d.smokes)+1.1])
+  .range([height, 0]);
+  
+// Create axis functions
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+
+//Append axes to the chart
+chartGroup.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(bottomAxis);
+
+chartGroup.append("g")
+  .call(leftAxis);
+
+//Create Circles
+var circlesGroup = chartGroup.selectAll("circle").data(data).enter();
+  
+var cTip=circlesGroup.append("circle")  
+  .classed("stateCircle", true)
+  .attr("cx", d => xLinearScale(d.poverty))
+  .attr("cy", d => yLinearScale(d.smokes))
+  .attr("r", "15")
+  .attr("opacity", ".5")
+  .attr("fill", "#f5f5dc");
+  
+//Create text labels with state abbreviation for each circle
+circlesGroup.append("text")
+  .classed("stateText", true)
+  .attr("x", d => xLinearScale(d.poverty))
+  .attr("y", d => yLinearScale(d.smokes))
+  .attr("stroke", "brown")
+  .attr("font-size", "10px")
+  .text(d => d.abbr)
+  .attr("fill", "#f5f5dc");
+    
+  
+//Initialize tool tip
+
+var toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([-8, 0])
+    .html(function(d) {
+      return (`${d.state}<br>In poverty: ${d.poverty}%<br>Smokes: ${d.smokes}%`);
+  });
+
+//Create tooltip in the chart
+cTip.call(toolTip);
+
+//Create event listeners to display and hide the tooltip
+cTip.on("mouseover", function(d) {
+  d3.select(this).style("stroke", "black")
+  toolTip.show(d, this);
+})
+  //onmouseout event
+  .on("mouseout", function(d, index) {
+    d3.select(this).style("stroke", "lightblue")
+    toolTip.hide(d);
+  });
+
+// Create axes labels
+chartGroup.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - margin.left + 40)
+.attr("x", 0 - (height / 2))
+.attr("dy", "1em")
+.attr("class", "aText")
+.text("Smokes (%)");
+
+chartGroup.append("text")
+.attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+.attr("class", "aText")
+.text("In Poverty (%)");
+    
 });
-var d = data;
-
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
-
-/* 
- * value accessor - returns the value to encode for a given data object.
- * scale - maps value to a visual display encoding, such as a pixel position.
- * map function - maps from data value to display value
- * axis - sets up axis
- */ 
-
-// setup x 
-var xValue = function(d) { return d.poverty;}, // data -> value
-    xScale = d3.scaleLinear().range([0, width]), // value -> display
-    xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.axisBottom(xScale);
-
-// setup y
-var yValue = function(d) { return d.smokes;}, // data -> value
-    yScale = d3.scaleLinear().range([height, 0]), // value -> display
-    yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis = d3.axisLeft(yScale);
-
-// setup fill color
-var cValue = function(d) { return d.obesity;},
-    color = d3.scaleOrdinal(d3.schemeCategory10);
-
-// add the graph canvas to the body of the webpage
-var svg = d3.select("#scatter").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// add the tooltip area to the webpage
-var tooltip = d3.select("#scatter").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-  // don't want dots overlapping axis, so add in buffer to data domain
-  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-  yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
-
-  // x-axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Poverty");
-
-  // y-axis
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Smokes");
-
-  // draw dots
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", xMap)
-      .attr("cy", yMap)
-      .style("fill", function(d) { return color(cValue(d));}) 
-      .on("mouseover", function(d) {
-          tooltip.transition()
-               .duration(200)
-               .style("opacity", .9);
-          tooltip.html(d.abbr + "<br/> (" + xValue(d) 
-	        + ", " + yValue(d) + ")")
-               .style("left", (d3.event.pageX + 5) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
-      })
-      .on("mouseout", function(d) {
-          tooltip.transition()
-               .duration(500)
-               .style("opacity", 0);
-      });
-
-  // draw legend
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  // draw legend text
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d;})
-// });
